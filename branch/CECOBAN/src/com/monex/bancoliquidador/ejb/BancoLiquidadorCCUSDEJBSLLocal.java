2c4971bc5cffd0ec8f378ee9dcebe39d01be64db
lamz <yire_777@hotmail.com>
2015-03-30 13:04:21 -0600

    Se borra carpeta Branch
    Ya que el mismo Repositorio lo hace

--- branch/CECOBAN/src/com/monex/bancoliquidador/ejb/BancoLiquidadorCCUSDEJBSLLocal.java
+++
@@ -1,9 +0,0 @@
-package com.monex.bancoliquidador.ejb;
-
-import javax.ejb.EJBLocalObject;
-import com.monex.comun.bd.MatrizDatos;
-
-public interface BancoLiquidadorCCUSDEJBSLLocal
-    extends EJBLocalObject {
-  public MatrizDatos getMonitorSaldos(String sCveEntidad, String sFecha);
-}
--- branch/CECOBAN/src/com/monex/bancoliquidador/ejb/BancoLiquidadorCCUSDEJBSLLocalHome.java
+++
@@ -1,7 +0,0 @@
-package com.monex.bancoliquidador.ejb;
-
-import javax.ejb.EJBLocalHome;
-
-public interface BancoLiquidadorCCUSDEJBSLLocalHome
-    extends EJBLocalHome {
-}
